8eecfd737e44ab055f3e95f8a623ac472d80447f
audreywelch <welch.audreyl@gmail.com>
2019-05-16 13:45:46 -0500

    Heap's get_max() and get_size() methods completed

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -2,20 +2,34 @@ class Heap:
   def __init__(self):
     self.storage = []
 
+  # Adds the input value into the heap, ensuring that the inserted value is in the correct spot in the heap
   def insert(self, value):
     pass
 
+  # Removes and returns the 'topmost' value from the heap, ensuring that the heap property is maintained after the topmost element has been removed
   def delete(self):
     pass
 
+  # Returns the maximum value in the heap in constant time
   def get_max(self):
-    pass
+    return self.storage[0]
 
+  # Returns the number of elements stored in the heap
   def get_size(self):
-    pass
 
+    count = 0
+
+    for each_element in self.storage:
+      count += 1
+
+    return count
+
+  # Moves the element at the specified index "up" the heap by swapping it with its parent if the parent's value is less than the value at the specified index.
   def _bubble_up(self, index):
     pass
 
+  # Grabs the indices of this element's children and determines which child has a larger value. If the larger child's value is larger than the parent's value, the child element is swapped with the parent
   def _sift_down(self, index):
     pass
+
+new_heap = Heap()
